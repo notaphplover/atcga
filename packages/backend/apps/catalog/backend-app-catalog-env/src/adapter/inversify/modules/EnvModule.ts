efa5bd46958fa277fffc67604615511ba0c12bf6
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-07-02 21:07:01 +0200

    feat(backend-app-catalog-env): update EnvModule with missing injection

--- packages/backend/apps/catalog/backend-app-catalog-env/src/adapter/inversify/modules/EnvModule.ts
+++ packages/backend/apps/catalog/backend-app-catalog-env/src/adapter/inversify/modules/EnvModule.ts
@@ -1,11 +1,13 @@
 import { ContainerModule, ContainerModuleLoadOptions } from 'inversify';
 
 import { EnvironmentLoader } from '../../../application/services/EnvironmentLoader';
+import { EnvironmentService } from '../../../application/services/EnvironmentService';
 
 export class EnvModule extends ContainerModule {
   constructor() {
     super((options: ContainerModuleLoadOptions) => {
       options.bind(EnvironmentLoader).toSelf().inSingletonScope();
+      options.bind(EnvironmentService).toSelf().inSingletonScope();
     });
   }
 }
